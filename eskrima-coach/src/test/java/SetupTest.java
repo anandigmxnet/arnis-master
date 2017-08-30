1fffbc52999338f1ffb53d16039fdcb1c6f80260
André Hanika <andre.hanika@inventage.com>
2017-08-30 16:50:27 +0200

    Add play test

--- eskrima-coach/src/test/java/SetupTest.java
+++ eskrima-coach/src/test/java/SetupTest.java
@@ -5,19 +5,31 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import org.junit.Test;
 
 public class SetupTest {
 
     @Test
-    public void readFolder() {
+    public void readFiles() throws IOException {
         Path resourceDirectory = Paths.get("src/test/resources");
-        try {
-            final List<Path> list = Files.list(resourceDirectory).map(path -> path.getFileName()).collect(Collectors.toList());
-            list.forEach(path -> System.out.println(path.getFileName()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        final List<Path> list = Files.list(resourceDirectory).collect(Collectors.toList());
+        list.forEach(path -> System.out.println(path.getFileName()));
         System.out.println(resourceDirectory.toAbsolutePath());
     }
+
+    @Test
+    public void playAudio() throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        Path resourceDirectory = Paths.get("src/test/resources");
+        final List<Path> list = Files.list(resourceDirectory).collect(Collectors.toList());
+        final Clip clip = AudioSystem.getClip();
+        clip.open(AudioSystem.getAudioInputStream(list.get(0).toFile()));
+        clip.start();
+    }
+
+    public void playList
 }
