cd8b4b19ad25747302786d0c117413ac9746a2c6
André Hanika <andre.hanika@inventage.com>
2017-09-05 17:20:46 +0200

    Introdduce command

--- eskrima-coach/src/test/java/SetupTest.java
+++ eskrima-coach/src/test/java/SetupTest.java
@@ -4,8 +4,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
@@ -30,38 +33,40 @@ public class SetupTest {
     }
 
     @Test
-    public void playList() throws IOException, LineUnavailableException {
-        final List<Path> paths = FileCollector.allFilesFrom("src/test/resources");
-        paths.
-        final Clip clip = AudioSystem.getClip();
-
-        final ArnisCoach arnisCoach = new ArnisCoach("test", paths);
+    public void playList() throws Exception {
+        final List<Path> list = FileCollector.allFilesFrom("src/test/resources");
+        final List<Command> audioFiles = createCommands(list);
+        final ArnisCoach arnisCoach = new ArnisCoach("test", audioFiles);
         arnisCoach.start();
 
     }
 
+    private List<Command> createCommands(List<Path> audioFiles) throws Exception {
+        final List<Command> commands = new LinkedList<>();
+        for (Path audioFile : audioFiles) {
+            Command command = new Command(audioFile);
+            command.initialize();
+            commands.add(command);
+        }
+        return commands;
+    }
+
     private class ArnisCoach extends Thread {
 
-        private List<Clip> filesToPlay = new LinkedList<>();
+        private List<Command> commands = new LinkedList<>();
 
         private Random random = new Random();
         private int delayInSec = 1;
+        private boolean shoutCommands = true;
 
-        public ArnisCoach(String name, List<Clip> filesToPlay) {
+        public ArnisCoach(String name, List<Command> commands) {
             super(name);
-            this.filesToPlay = filesToPlay;
+            this.commands = commands;
         }
 
-        private Clip chooseRandomClip() {
-            int randomIndex = random.nextInt(filesToPlay.size());
-            return filesToPlay.get(randomIndex);
-        }
-
-        private Clip createCommandFrom(Path path) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
-            final Clip clip = AudioSystem.getClip();
-            clip.open(AudioSystem.getAudioInputStream(path.toFile()));
-            return clip;
-
+        private Command chooseRandomClip() {
+            int randomIndex = random.nextInt(commands.size());
+            return commands.get(randomIndex);
         }
 
         public void setDelayInSec(int delayInSec) {
@@ -70,22 +75,84 @@ public class SetupTest {
 
         @Override
         public void run() {
-            final Clip clip = chooseRandomClip();
-            try {
-                System.out.println("Start clip");
-                clip.start();
-
-                while(clip.isActive()) {
-                    System.out.println("playing...");
-                    //nop
+            System.out.println("start coach");
+            while (shoutCommands) {
+                final Command command = chooseRandomClip();
+                try {
+                    System.out.println("start command");
+                    command.execute();
+                    System.out.println("stop command");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                System.out.println("End clip");
 
-                sleep(delayInSec * 1000);
-            } catch (Exception e) {
-                e.printStackTrace();
+
             }
+            System.out.println("stop coach");
 
         }
     }
+
+
+    private class Command {
+        private final Path audioFile;
+        private final Clip clip;
+        private final AudioListener audioListener;
+
+        public Command(Path audioFile) throws Exception {
+            this.audioFile = audioFile;
+            this.clip = AudioSystem.getClip();;
+            this.audioListener = new AudioListener();
+
+        }
+
+        public void initialize() throws Exception {
+            final AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile.toFile());
+            clip.open(audioInputStream);
+            clip.addLineListener(audioListener);
+        }
+
+
+        public void execute() throws InterruptedException {
+                clip.start();
+                audioListener.waitUntilDone();
+                clip.stop();
+        }
+    }
+
+
+    private class AudioListener implements LineListener {
+        private boolean done = false;
+        @Override public synchronized void update(LineEvent event) {
+            LineEvent.Type eventType = event.getType();
+            if (eventType == LineEvent.Type.STOP || eventType == LineEvent.Type.CLOSE) {
+                done = true;
+                notifyAll();
+            }
+        }
+        public synchronized void waitUntilDone() throws InterruptedException {
+            while (!done) { wait(); }
+        }
+    }
 }
+
+
+//    private static void playClip(File clipFile) throws IOException,
+//            UnsupportedAudioFileException, LineUnavailableException, InterruptedException {
+//
+//        AudioListener listener = new AudioListener();
+//        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(clipFile);
+//        try {
+//            Clip clip = AudioSystem.getClip();
+//            clip.addLineListener(listener);
+//            clip.open(audioInputStream);
+//            try {
+//                clip.start();
+//                listener.waitUntilDone();
+//            } finally {
+//                clip.close();
+//            }
+//        } finally {
+//            audioInputStream.close();
+//        }
+//    }
